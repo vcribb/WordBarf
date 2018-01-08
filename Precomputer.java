7436c3ef42003204efd901d9904f3e7665aa9072
horazio <hfusco@stuy.edu>
2018-01-08 18:55:50 -0500

    working on the precomputer; it worksgit add . It can find all the words adjacent to a given word, now we need to change the way it is presented and put it in a new file

--- Precomputer.java
+++ Precomputer.java
@@ -6,7 +6,7 @@ public class Precomputer{
     public static boolean adjacent(String a, String b){
 	int count = 0;
 	for(int i = 0; i < a.length(); i ++){
-	    if(a.charAt(i) == b.charAt(i)){
+	    if(a.charAt(i) != b.charAt(i)){
 		count++;
 	    }
 	    
