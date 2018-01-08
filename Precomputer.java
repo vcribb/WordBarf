12c1de0291ec7347dbcdcbefd07027b6e78e21ef
horazio <hfusco@stuy.edu>
2018-01-08 15:26:18 -0500

    working on precomputer; tinkering with parcing the list of words for adjacent words, working through errors

--- Precomputer.java
+++ Precomputer.java
@@ -2,6 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class Precomputer{
+
+    public static boolean adjacent(String a, String b){
+	int count = 0;
+	for(int i = 0; i < a.length(); i ++){
+	    if(a.charAt(i) == b.charAt(i)){
+		count++;
+	    }
+	    
+	}
+
+	return count == 1;
+
+    }
+
+
+
+    
     public static void main(String[] args){
 	try{
 	ArrayList<String> ans = new ArrayList<String>();
@@ -20,15 +37,25 @@ public class Precomputer{
 	    
 	    
 	}
+    
+    
+    
+    for(int i = 0; i < ans.size(); i++){
+	String wordsForI = "";
+	for ( int j = 0; j < ans.size(); j++){
+		if( adjacent(ans.get(i), ans.get(j))){
+		    wordsForI += ans.get(j) + ' ';
+		}
+		    
+	       
+	    }
+	System.out.println(ans.get(i) + " : " + wordsforI); 
     }
     
-    
-    // for(int i = 0; i <
-    
 
 
 
-
+    }
 
 
 
