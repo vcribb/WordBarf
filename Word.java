317b3bb51cdce2196797e21f26cb746729f37313
horazio <hfusco@stuy.edu>
2018-01-09 18:05:51 -0500

    copy and pasted vivians Word file into my Word file

---
+++ Word.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class Word{
+    private String word;
+    private Word[] linksTo;
+    private boolean visited;
+
+    public Word(){
+        visited = false;
+    }
+
+
+    public boolean isVisited(){
+        return visited;
+    }
+
+    
+    public void visit(){
+        visited = true;
+    }
+
+
+    public String getWord(){
+        return word;
+    }
+
+    public Word nextWord(String s){
+        return linksTo[0];//goes through linksTo and finds the most similar word
+    }
+
+    public void makeLinks(int[] indices, Word[] wordList){
+       for(int i = 0; i < indices.length; i++){
+           linksTo[i] = wordList[indices[i]];
+       }   
+    }
+    
+    public int[] parseLine(String line){
+        word = line.substring(0,4);
+        line = line.substring(4,line.length());
+        String[] wordFam = line.split(" ");
+        int[] wordFam2 = new int[wordFam.length];
+        for(int x = 0; x < wordFam.length; x++){
+            wordFam2[x] = Integer.parseInt(wordFam[x]);
+	    
+        }
+        return wordFam2;
+    }
+    
+    public static void main (String[] args){
+        Word[] words = new Word[2404];
+       
+     
+        try{
+
+            File f = new File("adjacentWords.txt");
+            Scanner reader = new Scanner(f);
+            int i = 0;
+            while(reader.hasNext()){
+                words[i] = new Word();              
+                words[i].makeLinks(words[i].parseLine(reader.nextLine()));
+                i++;
+            }
+        }catch(FileNotFoundException e){
+            
+        }
+ 
+        
+        for(int i = 0; i < words.length; i++){
+           System.out.println(words[i].getWord());
+        }
+    
+    
+    Word robert = new Word("fish");
+
+
+	
+    }
+}
