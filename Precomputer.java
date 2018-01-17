76cc7d669e68a23f0ca582e8fef6637c25387346
Vivian <vcribb@stuy.edu>
2018-01-17 07:29:03 -0500

    added a check for adjacent words with different lengths.  need to test and figure out what's wrong with the list

--- Precomputer.java
+++ Precomputer.java
@@ -6,9 +6,43 @@ public class Precomputer{
     /*determines if two words have a difference of exactly one letter*/
     public static boolean adjacent(String a, String b){
 	if (a.length() != b.length()){
-        return false;
-    }
-    int count = 0;
+	    if (a.length() == b.length()-1){
+		int badindex = a.length();
+		for (int i = 0; i < a.length(); i++){
+		    if (a.charAt(i) != b.charAt(i)){
+			badindex = i;
+		    }
+		}
+		String s = b.substring(0,badindex) +
+		    b.substring(badindex + 1,b.length());
+		int count = 0;
+		for (int i = 0; i < a.length(); i++){
+		    if (a.charAt(i) != s.charAt(i)){
+			count++;
+		    }
+		}
+		return count == 0;
+	    }
+	    if (b.length() == a.length()-1){
+		int badindex = b.length();
+		for (int i = 0; i < b.length(); i++){
+		    if (a.charAt(i) != b.charAt(i)){
+			badindex = i;
+		    }
+		}
+		String s = a.substring(0,badindex) +
+		    a.substring(badindex + 1,a.length());
+		int count = 0;
+		for (int i = 0; i < b.length(); i++){
+		    if (s.charAt(i) != b.charAt(i)){
+			count++;
+		    }
+		}
+		return count == 0;
+	    }
+	    return false;
+	}
+	int count = 0;
                 
 	//count determines how many letters the two strings have in common
 	for(int i = 0; i < a.length(); i ++){
@@ -26,8 +60,8 @@ public class Precomputer{
 	    Scanner reader = new Scanner(f);
 	    while(reader.hasNext()){
 		if( reader.next().length() == 2){
-            ans.add(reader.next());
-        }
+		    ans.add(reader.next());
+		}
 	    }
 	    System.out.println(ans);
 	    //copies the five-letter English words into ans
